7cb10300b16ac34b63428930da270d6d76961d70
Nikita228133769 <nikita0zrz@gmail.com>
2023-03-21 14:25:21 +0300

    Update Program.cs

--- FBXSample/Program.cs
+++ FBXSample/Program.cs
@@ -12,29 +12,32 @@ namespace FBXSample
         {
             AssimpContext importer = new AssimpContext();
 
-            Assimp.Scene model_1 = importer.ImportFile(file.fbx, PostProcessPreset.TargetRealTimeMaximumQuality);
-            var a = model_1.RootNode.Children;
+            Assimp.Scene model_assimp = importer.ImportFile(file.fbx, PostProcessPreset.TargetRealTimeMaximumQuality);
+            foreach(var child in model_assimp.RootNode.Children)
+            {
+                var mesh = child.MeshIndices;
+            }
 
-            var model = FbxIO.Read(file.fbx);
-            var geometryIds = model.GetGeometryIds();
+            var model_sharpie = FbxIO.Read(file.fbx);
+            var geometryIds = model_sharpie.GetGeometryIds();
             foreach (var geometryId in geometryIds)
             {
-                var vertexIndices = model.GetVertexIndices(geometryId);
-                var positions = model.GetPositions(geometryId, vertexIndices);
+                var vertexIndices = model_sharpie.GetVertexIndices(geometryId);
+                var positions = model_sharpie.GetPositions(geometryId, vertexIndices);
 
-                var normalLayerIndices = model.GetLayerIndices(geometryId, FbxLayerElementType.Normal);
-                var tangentLayerIndices = model.GetLayerIndices(geometryId, FbxLayerElementType.Tangent);
-                var binormalLayerIndices = model.GetLayerIndices(geometryId, FbxLayerElementType.Binormal);
-                var texCoordLayerIndices = model.GetLayerIndices(geometryId, FbxLayerElementType.TexCoord);
-                var materialLayerIndices = model.GetLayerIndices(geometryId, FbxLayerElementType.Material);
+                var normalLayerIndices = model_sharpie.GetLayerIndices(geometryId, FbxLayerElementType.Normal);
+                var tangentLayerIndices = model_sharpie.GetLayerIndices(geometryId, FbxLayerElementType.Tangent);
+                var binormalLayerIndices = model_sharpie.GetLayerIndices(geometryId, FbxLayerElementType.Binormal);
+                var texCoordLayerIndices = model_sharpie.GetLayerIndices(geometryId, FbxLayerElementType.TexCoord);
+                var materialLayerIndices = model_sharpie.GetLayerIndices(geometryId, FbxLayerElementType.Material);
 
-                var normals = model.GetNormals(geometryId, vertexIndices, normalLayerIndices[0]);
+                var normals = model_sharpie.GetNormals(geometryId, vertexIndices, normalLayerIndices[0]);
 
-                var hasNormals = model.GetGeometryHasNormals(geometryId);
-                var hasTexCoords = model.GetGeometryHasTexCoords(geometryId);
-                var hasTangents = model.GetGeometryHasTangents(geometryId);
-                var hasBinormals = model.GetGeometryHasBinormals(geometryId);
-                var hasMaterials = model.GetGeometryHasMaterials(geometryId);
+                var hasNormals = model_sharpie.GetGeometryHasNormals(geometryId);
+                var hasTexCoords = model_sharpie.GetGeometryHasTexCoords(geometryId);
+                var hasTangents = model_sharpie.GetGeometryHasTangents(geometryId);
+                var hasBinormals = model_sharpie.GetGeometryHasBinormals(geometryId);
+                var hasMaterials = model_sharpie.GetGeometryHasMaterials(geometryId);
             }
         }
     }
